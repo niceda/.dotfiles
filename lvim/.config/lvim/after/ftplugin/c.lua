49cb2892cd952d6e09e6aba52c90c27cb12531b3
Fida <fida@ximix.localdomain>
2023-02-23 21:52:12 +0800

    c/cpp

--- lvim/.config/lvim/after/ftplugin/c.lua
+++ lvim/.config/lvim/after/ftplugin/c.lua
@@ -1,3 +1,5 @@
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4 -- insert 2 spaces for a tab
 -- some settings can only passed as commandline flags `clangd --help`
 local clangd_flags = {
 	"--all-scopes-completion",
