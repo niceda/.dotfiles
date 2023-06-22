13ec21f1b8df272c589e2ba902469fd5df099a4c
Fida <iwanderuu@gmail.com>
2023-06-22 13:40:53 +0800

    解决wsl和windows粘贴文本后带^M的问题

--- lvim/.config/lvim/lua/user/settings.lua
+++ lvim/.config/lvim/lua/user/settings.lua
@@ -1,2 +1,15 @@
 lvim.builtin.treesitter.rainbow.enable = true
-vim.opt.spelllang = "en,cjk"
+
+-- Set a compatible clipboard manager
+if vim.fn.has("wsl") == 1 or vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+	vim.g.clipboard = {
+		copy = {
+			["+"] = "win32yank.exe -i --crlf",
+			["*"] = "win32yank.exe -i --crlf",
+		},
+		paste = {
+			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
+		},
+	}
+end
