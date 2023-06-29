334c406c22d459d6017bbe114ff364cffae0560f
Fida <iwanderuu@gmail.com>
2023-06-29 15:17:00 +0800

    rainbow

--- lvim/.config/lvim/lua/user/settings.lua
+++ lvim/.config/lvim/lua/user/settings.lua
@@ -1,3 +1,20 @@
+lvim.builtin.treesitter.rainbow = {
+	enable = true,
+	query = {
+		"rainbow-parens",
+	},
+	strategy = require("ts-rainbow").strategy.global,
+	-- hlgroups = {
+	-- 	-- "TSRainbowRed",
+	-- 	"TSRainbowBlue",
+	-- 	-- "TSRainbowOrange",
+	-- 	-- "TSRainbowCoral",
+	-- 	-- "TSRainbowPink",
+	-- 	"TSRainbowYellow",
+	-- 	-- "TSRainbowViolet",
+	-- 	"TSRainbowGreen",
+	-- },
+}
 -- Set a compatible clipboard manager
 if vim.fn.has("wsl") == 1 or vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
 	vim.g.clipboard = {
