299a008e5e2cd722b47b46cc81eb5ff064de934e
XiongDajun <iwanderuu@gmail.com>
2023-09-14 17:43:04 +0800

    update

--- lvim/.config/lvim/lua/user/plugins.lua
+++ lvim/.config/lvim/lua/user/plugins.lua
@@ -395,12 +395,14 @@ lvim.plugins = {
 							-- You can use the capture groups defined in textobjects.scm
 							["af"] = "@function.outer",
 							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
+							-- ["ac"] = "@class.outer",
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
 							-- You can optionally set descriptions to the mappings (used in the desc parameter of
 							-- nvim_buf_set_keymap) which plugins like which-key display
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+							-- ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
 							-- You can also use captures from other query groups like `locals.scm`
-							["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+							-- ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
 						},
 						-- You can choose the select mode (default is charwise 'v')
 						--
@@ -425,15 +427,15 @@ lvim.plugins = {
 						-- and should return true of false
 						include_surrounding_whitespace = true,
 					},
-					swap = {
-						enable = false,
-						swap_next = {
-							["<leader>a"] = "@parameter.inner",
-						},
-						swap_previous = {
-							["<leader>A"] = "@parameter.inner",
-						},
-					},
+					-- swap = {
+					-- 	enable = false,
+					-- 	swap_next = {
+					-- 		["<leader>a"] = "@parameter.inner",
+					-- 	},
+					-- 	swap_previous = {
+					-- 		["<leader>A"] = "@parameter.inner",
+					-- 	},
+					-- },
 					move = {
 						enable = true,
 						set_jumps = true, -- whether to set jumps in the jumplist
@@ -610,6 +612,6 @@ lvim.plugins = {
 			})
 		end,
 	},
-  "olexsmir/gopher.nvim",
-  "leoluz/nvim-dap-go",
+	"olexsmir/gopher.nvim",
+	"leoluz/nvim-dap-go",
 }
