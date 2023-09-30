3080880361731028644db44cb962b2d4dc34e5f8
XiongDajun <iwanderuu@gmail.com>
2023-09-30 11:46:01 +0800

    add go path

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -8,11 +8,14 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 export HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/.zsh_history"
 
+export GOPATH=$HOEM/go
+export PATH=$PATH:$HOME/go/bin
+
 export ZFM_NO_BINDINGS=1
 
 # 轻舟
-export http_proxy=http://10.4.1.37:7890
-export https_proxy=http://10.4.1.37:7890
+# export http_proxy=http://10.4.1.37:7890
+# export https_proxy=http://10.4.1.37:7890
 # 家
-# export http_proxy=http://192.168.1.194:7890
-# export https_proxy=http://192.168.1.194:7890
+export http_proxy=http://192.168.1.194:7890
+export https_proxy=http://192.168.1.194:7890
