4f1a81d5efcfcc0afbf290fca62badafb90cd569
xiongdajun <iwanderuu@gmail.com>
2024-06-03 10:58:50 +0800

    update

--- docker/debian11.7-amd64/install.sh
+++ docker/debian11.7-amd64/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker build -t niceda/debian:11.7-arm .
+docker build -t niceda/debian:11.7 .
 
 docker run -d --platform linux/amd64 --name dev -p 2222:22 -p 8000:8000 -p 8001:8001 -p 8002:8002 --privileged=true -v "$HOME/Documents/Code/qz/":/Users/fida/Documents/Code/qz -v "$HOME/.dotfiles/":/home/fida/.dotfiles niceda/debian:11.7
-docker run -d --name dev -p 2222:22 -p 8000:8000 -p 8001:8001 -p 8002:8002 --privileged=true -v "$HOME/Documents/Code/qz/":/Users/fida/Documents/Code/qz -v "$HOME/.dotfiles/":/home/fida/.dotfiles niceda/debian:11.7-arm
+docker run -d --name dev -p 2222:22 -p 8000:8000 -p 8001:8001 -p 8002:8002 --privileged=true -v "$HOME/Documents/Code/qz/":/Users/fida/Documents/Code/qz -v "$HOME/.dotfiles/":/home/fida/.dotfiles niceda/debian:11.7
 
 echo `export PATH=$HOME/.local/bin:$PATH` >> /$HOME/.bashrc
 source ~/.bashrc
