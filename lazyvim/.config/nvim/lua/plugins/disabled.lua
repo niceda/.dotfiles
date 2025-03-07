1845003514894ed0090f850809986acd4faffffe
xiongdajun <iwanderuu@gmail.com>
2025-03-07 09:23:41 +0800

    cursor

--- lazyvim/.config/nvim/lua/plugins/disabled.lua
+++ lazyvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,3 +1,10 @@
+if vim.g.vscode then
+  return {
+    { "mini.animate", enabled = false },
+    { "dashboard-nvim", enabled = false },
+  }
+end
+
 return {
   { "dashboard-nvim", enabled = false },
 }
