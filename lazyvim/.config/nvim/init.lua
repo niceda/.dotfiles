af6fac1ecbe1f72d899d277ce92becb50eaeb491
xiongdajun <iwanderuu@gmail.com>
2025-04-15 16:25:58 +0800

    keymap

--- lazyvim/.config/nvim/init.lua
+++ lazyvim/.config/nvim/init.lua
@@ -1,6 +1,8 @@
 if vim.g.vscode then
-  vim.keymap.set({ "i", "n" }, "<esc><esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
+  vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
   vim.keymap.set("n", "gr", [[<cmd>lua require('vscode').action('editor.action.referenceSearch.trigger')<cr>]])
+  vim.keymap.set("n", "gI", [[<cmd>lua require('vscode').action('editor.action.goToImplementation')<cr>]])
+  vim.keymap.set("n", "gy", [[<cmd>lua require('vscode').action('editor.action.goToTypeDefinition')<cr>]])
 end
 
 require("config.commands")
